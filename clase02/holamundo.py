c75db6606453362668d160ba6d41f864280261af
Andre Cruz <71595562@certus.edu.pe>
2021-04-16 22:14:30 -0500

    primeros pasos de programacion

--- clase02/holamundo.py
+++ clase02/holamundo.py
@@ -1,1 +1,41 @@
-print('hola mundo')
+#def print_holamundo(texto,edad):
+#    print("Hola "+texto)
+
+#print_holamundo("mundo")
+print("hola mundo")
+edad = 26
+print('tengo', edad, 'años')
+
+base = 10
+height = 3
+area = base*height
+print('el area es:', area)
+
+#suma
+a = 5
+b = 6
+print('los numeros son:', a, ',', b)
+suma = a + b
+print('suma:',suma)
+#resta
+resta = a - b
+print('resta:',resta)
+
+#multiplicacion
+mult= a * b
+print('multiplicacion:', mult)
+#division
+division = a/b
+print('division', division)
+#potencia
+potencia=a ** b
+print('potencia:', potencia)
+#modulo
+modulo=b % a
+print('modulo:', modulo)
+
+caracteres = len("hola mundo")
+print(caracteres)
+
+name = input("¿Cual es tu nombre? \n")
+print("Hola", name)
